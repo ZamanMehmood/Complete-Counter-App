6f64538f52036010723d0b27a925a740c93c2adf
Saqib <zaman342003@gmail.com>
2022-04-23 18:16:46 +0500

    Updates

---
+++ precache-manifest.be215c697438ef9bd5930409dd04f9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "065426b1fc92dcc63db3",
+    "url": "/Complete-Counter-App/static/css/main.fde4b72e.chunk.css"
+  },
+  {
+    "revision": "065426b1fc92dcc63db3",
+    "url": "/Complete-Counter-App/static/js/main.065426b1.chunk.js"
+  },
+  {
+    "revision": "48e425e0b3059097f74a",
+    "url": "/Complete-Counter-App/static/css/1.f1dedef6.chunk.css"
+  },
+  {
+    "revision": "48e425e0b3059097f74a",
+    "url": "/Complete-Counter-App/static/js/1.48e425e0.chunk.js"
+  },
+  {
+    "revision": "cc433fcc1dbebd4ef559",
+    "url": "/Complete-Counter-App/static/js/runtime~main.cc433fcc.js"
+  },
+  {
+    "revision": "98b79ad61f6351f168fd28e24fb4ea52",
+    "url": "/Complete-Counter-App/index.html"
+  }
+];
